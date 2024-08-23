98adbe59708ba44a8476490902caa716dac51f95
jayakrishnagaddam <2100090162@kluniversity.in>
2024-08-23 17:39:21 +0530

    Optimization

--- moviemodel.py
+++ moviemodel.py
@@ -5,10 +5,10 @@ import numpy as np
 from keras import layers
 
 # Load the dataset with a limit of the top 2,000 most frequent words
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=15000)
 
 # Set the dimensions to 2000
-dimensions = 10000
+dimensions = 15000
 
 def preprocess(sequences, dimensions):
     results = np.zeros((len(sequences), dimensions))
